bd03a80b18e049246c2905598ed4f9bed37c7494
Erico <erico.breyer08@gmail.com>
2025-06-17 22:05:32 -0300

    Basic player phisics

--- src/main.cpp
+++ src/main.cpp
@@ -411,17 +411,46 @@ int main(int argc, char* argv[])
             // controladas pelo mouse do usuário. Veja as funções CursorPosCallback()
             // e ScrollCallback().
             float r = g_CameraDistance;
-            float y = r*sin(g_CameraPhi);
+            float y = r*-sin(g_CameraPhi);
             float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
             float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
-            // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
+
+             // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
             // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-            glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-            glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+            glm::vec4 camera_position_c  = glm::vec4(0.0f,0.1f,0.0f,1.0f) + delta_camera; // Ponto "c", centro da câmera
+            glm::vec4 camera_lookat_l    = glm::vec4(x,y,z,1.0f) + delta_camera; // Ponto "l", para onde a câmera (look-at) estará sempre olhando
             glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
             glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
+            glm::vec4 w = -camera_view_vector;
+            glm::vec4 u = crossproduct(camera_up_vector,w);
+    
+            // Normalizamos os vetores u e w            
+            w.y = 0.0f;
+            u.y = 0.0f;
+            w = w/norm(w);
+            u = u/norm(u);
+            float currentTime = (float)glfwGetTime();
+            float deltaTime = currentTime - g_LastFrameTime;
+            g_LastFrameTime = currentTime;
+
+            
+            float speed = 2 * deltaTime;
+            if(g_WPressed){
+                delta_camera-= w*speed;
+            }
+            if(g_APressed){
+                delta_camera -= u*speed;
+            }
+            if(g_DPressed){
+                delta_camera += u*speed;
+            }
+            if(g_SPressed){
+                delta_camera += w*speed;
+            }
+
             view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+            player_position = camera_position_c;
         }
         else
         {
